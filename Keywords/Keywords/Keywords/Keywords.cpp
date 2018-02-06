31c9de084455550fc368cf3dfc5294c8471c6133
Christopher D Koon <CHRKOON@uat.edu>
2018-02-06 13:01:30 -0700

    Add files via upload

---
+++ Keywords/Keywords/Keywords/Keywords.cpp
@@ -0,0 +1,24 @@
+// Keywords.cpp : Defines the entry point for the console application.
+/* The keywords are: Bomb
+					 Attack
+					 Boom
+					 Hack
+					 Airplane
+					 Charge
+					 Destroy
+					 Government
+					 Base
+					 Operation*/
+
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
+
